93294f0f2b010ae4728c6389ae79008c67a53b3a
Bas <8457098+BasVanSch1@users.noreply.github.com>
2022-12-09 10:51:19 +0100

    Assembly updates

--- CEFotoDownloader/AssemblyInfo.cs
+++ CEFotoDownloader/AssemblyInfo.cs
@@ -10,14 +10,14 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Programma voor het downloadn van artikel foto's van de webshop")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Programma voor het downloaden van artikel foto's van de webshop")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bas van Schoubroeck")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clayre & Eef Webfoto Downloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEFotoDownloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0")]
-[assembly: System.Resources.NeutralResourcesLanguageAttribute("nl")]
+[assembly: System.Resources.NeutralResourcesLanguageAttribute("NL-nl")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
